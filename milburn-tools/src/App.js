56d04ac895a0e1abd11a7d5e48b56156b2d6ed12
rafidmahin20 <submanmahin@gmail.com>
2022-05-22 12:35:55 +0600

    edited

--- milburn-tools/src/App.js
+++ milburn-tools/src/App.js
@@ -1,4 +1,6 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
@@ -6,6 +8,9 @@ function App() {
   return (
     <div>
      <Header></Header>
+     <Routes>
+       <Route path='/' element={<Home/>}/>
+     </Routes>
      <Footer></Footer>
     </div>
   );
--- milburn-tools/src/Pages/Home/Home.js
+++ milburn-tools/src/Pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
-            
+            <Helmet>
+                <title>Home - Milburn Tools</title>
+            </Helmet>
         </div>
     );
 };
