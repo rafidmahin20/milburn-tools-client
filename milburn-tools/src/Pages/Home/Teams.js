6d8957a3f81957d3456f7cde435b86078e3beff9
rafidmahin20 <submanmahin@gmail.com>
2022-05-22 13:41:01 +0600

    edited

--- milburn-tools/src/Pages/Home/Teams.js
+++ milburn-tools/src/Pages/Home/Teams.js
@@ -19,9 +19,9 @@ const Teams = () => {
                     <div className="flex flex-col items-center p-8 transition-colors duration-200 transform border cursor-pointer rounded-xl hover:border-transparent group hover:bg-blue-600 dark:border-gray-700 dark:hover:border-transparent">
                         <img className="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300" src={tony} alt=""/>
                         
-                        <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white group-hover:text-white">Tony Stark</h1>
+                        <h1 className="mt-4 text-2xl font-semibold capitalize dark:text-red-600 group-hover:text-white">Tony Stark</h1>
                         
-                        <p className="mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300">design director</p>
+                        <p className="mt-2 text-gray-500 capitalize dark:text-black group-hover:text-gray-300">design director</p>
                         
                         <div className="flex mt-3 -mx-2">
                             <p href="#" className="mx-2 text-gray-600 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-300 group-hover:text-white" aria-label="Reddit">
