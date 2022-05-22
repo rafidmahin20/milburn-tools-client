f3e8c58c0898a180087637aae489b69e25ad01b4
rafidmahin20 <submanmahin@gmail.com>
2022-05-22 13:08:53 +0600

    edited

--- milburn-tools/src/App.js
+++ milburn-tools/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Pages/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
      <Header></Header>
      <Routes>
        <Route path='/' element={<Home/>}/>
+       <Route path='*' element={<NotFound/>}/>
      </Routes>
      <Footer></Footer>
     </div>
--- milburn-tools/src/Pages/Home/Banner.js
+++ milburn-tools/src/Pages/Home/Banner.js
@@ -3,13 +3,13 @@ import banner from '../../Images/banner.png'
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen">
-        <div class="hero-content flex-col lg:flex-row">
-          <img src={banner} class="max-w-sm rounded-lg shadow-2xl" alt=''/>
+        <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={banner} className="max-w-sm rounded-lg shadow-2xl" alt=''/>
           <div>
-            <h1 class="text-5xl font-bold">Hammer your way!</h1>
-            <p class="py-6">hammer, tool designed for pounding or delivering repeated blows.</p>
-            <button class="btn btn-primary">Get Started</button>
+            <h1 className="text-5xl font-bold">Hammer your way!</h1>
+            <p className="py-6">hammer, tool designed for pounding or delivering repeated blows.</p>
+            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
