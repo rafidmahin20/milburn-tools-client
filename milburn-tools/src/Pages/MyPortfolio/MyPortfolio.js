d52d0755903f5e81bfd20c6e0479fb775b810ce9
rafidmahin20 <submanmahin@gmail.com>
2022-05-23 00:23:08 +0600

    edited

--- milburn-tools/src/Pages/MyPortfolio/MyPortfolio.js
+++ milburn-tools/src/Pages/MyPortfolio/MyPortfolio.js
@@ -145,18 +145,18 @@ const MyPortfolio = () => {
       <div className="bg-gray-300 w-2 h-2 rounded-full -ml-1 md:ml-0 mr-3 md:mr-0 md:-mt-1"></div>
     </div>
     <div className="mt-0.5 ml-4 md:ml-0 pb-5">
-      <h4 className="text-gray-800 font-semibold text-xl mb-1.5">Title of section 2</h4>
-      <p className="text-gray-500 mb-3">Libero expedita explicabo eius fugiat quia aspernatur autem laudantium error architecto recusandae natus sapiente sit nam eaque, consectetur porro molestiae ipsam an deleniti.</p>
+      <h4 className="text-gray-800 font-semibold text-xl mb-1.5">CSS</h4>
+      <p className="text-gray-500 mb-3">I have the basic knowledge of css. And also have the knowledge of tailwind and bootstrap css framework. But i prefer tailwind. I like to work with tailwind framework very very much.</p>
     </div>
   </li>
   <li>
     <div className="flex md:block flex-start items-center pt-2 md:pt-0">
       <div className="bg-gray-300 w-2 h-2 rounded-full -ml-1 md:ml-0 mr-3 md:mr-0 md:-mt-1"></div>
-      <p className="text-gray-500 text-sm mt-2">25.11.2021</p>
     </div>
     <div className="mt-0.5 ml-4 md:ml-0 pb-5">
-      <h4 className="text-gray-800 font-semibold text-xl mb-1.5">Title of section 3</h4>
-      <p className="text-gray-500 mb-3">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+      <h4 className="text-gray-800 font-semibold text-xl mb-1.5">Javascript</h4>
+      <p className="text-gray-500 mb-3">I have the basic knowledge of js language. I solved many prolblems using js and also made websites using js. I use react as js library. i have the basic knowledge of react library.
+      </p>
     </div>
   </li>
 </ol>
