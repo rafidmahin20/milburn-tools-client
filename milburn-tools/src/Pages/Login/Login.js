7eb1851fc64d54e853c3fd7acfbb22e1209cca0c
rafidmahin20 <submanmahin@gmail.com>
2022-05-23 21:32:00 +0600

    edited

--- milburn-tools/src/Pages/Login/Login.js
+++ milburn-tools/src/Pages/Login/Login.js
@@ -44,11 +44,11 @@ const Login = () => {
 
             <form onSubmit={handleLogin}>
                 <div className="w-full mt-4">
-                    <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address" ref={emailRef}/>
+                    <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address" ref={emailRef} required/>
                 </div>
 
                 <div className="w-full mt-4">
-                    <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" ref={passRef}/>
+                    <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" ref={passRef} required/>
                 </div>
 
                 <div className="flex items-center justify-between mt-4">
