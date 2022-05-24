c97e2ca908f205ae6b0cc8da7cb317e599b086b9
rafidmahin20 <submanmahin@gmail.com>
2022-05-24 22:07:53 +0600

    edited

---
+++ milburn-tools/src/Pages/Hooks/useTool.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+const useTool = () => {
+    const [tools, setTools] = useState([]);
+    useEffect(() =>{
+        fetch('http://localhost:5000/tool')
+        .then(res => res.json())
+        .then(data => setTools(data))
+    }, [tools])
+    return [tools]
+};
+
+export default useTool;
