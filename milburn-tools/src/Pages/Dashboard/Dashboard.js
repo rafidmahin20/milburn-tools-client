3d178780563749774814fd4d7979d47122ae2c97
rafidmahin20 <submanmahin@gmail.com>
2022-05-25 10:45:10 +0600

    edited

--- milburn-tools/src/Pages/Dashboard/Dashboard.js
+++ milburn-tools/src/Pages/Dashboard/Dashboard.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-    return (
-        <div>
-           <h1>Dashboard</h1> 
+  return (
+    <section>
+        <Helmet>
+            <title>Dashboard - Milburn Tools</title>
+        </Helmet>
+      <div class="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col items-center justify-center">
+          <Outlet></Outlet>
+          <label
+            for="my-drawer-2"
+            class="btn btn-primary drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
         </div>
-    );
+        <div class="drawer-side">
+          <label for="my-drawer-2" class="drawer-overlay"></label>
+          <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
