6b0b1af25efba05d40dba265a2d9f3905e0c4bb5
rafidmahin20 <submanmahin@gmail.com>
2022-05-25 12:30:22 +0600

    edited

--- milburn-tools/src/Pages/Dashboard/AddReview.js
+++ milburn-tools/src/Pages/Dashboard/AddReview.js
@@ -22,8 +22,8 @@ const AddReview = () => {
     return (
         
    <form onSubmit={handleSubmit(onSubmit)}>
-       <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="number" placeholder="put rating" aria-label="Email Address"  required/>
-       <textarea className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address"  required/>
+       <input name='number' {...register("number")}className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="number" placeholder="put rating" aria-label="Email Address"  required/>
+       <textarea name='text' {...register("text")} className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="text" placeholder="add your review" aria-label="Email Address"  required/>
        <div className="flex items-center justify-between mt-4">
        <button className="px-4 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none" type="submit">Submit</button>
        </div>
