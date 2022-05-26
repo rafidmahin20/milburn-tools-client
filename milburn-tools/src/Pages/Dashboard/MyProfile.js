3f6203e93c55820b5c946bf74cb1a9e9a2b447fe
rafidmahin20 <submanmahin@gmail.com>
2022-05-26 15:48:53 +0600

    edited

--- milburn-tools/src/Pages/Dashboard/MyProfile.js
+++ milburn-tools/src/Pages/Dashboard/MyProfile.js
@@ -1,130 +1,161 @@
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import auth from "../Firebase.init";
 
 const MyProfile = () => {
   const [user] = useAuthState(auth);
+  const { handleSubmit, register } = useForm();
+  const navigate = useNavigate();
+  const onSubmit = (data) => {
+    const url = `https://ancient-falls-05343.herokuapp.com/profile`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        navigate("/");
+      });
+  };
   return (
     <section className="w-full max-w-2xl px-6 py-4 mx-auto bg-white rounded-md shadow-md mt-10">
-      <h2 className="text-3xl font-semibold text-center text-gray-800 dark:text-black">
-        My Profile
-      </h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2 className="text-3xl font-semibold text-center text-gray-800 dark:text-black">
+          My Profile
+        </h2>
 
-      <div className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3">
-        <p
-          href="#"
-          className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-black hover:bg-blue-200 dark:hover:bg-blue-500"
-        >
-          <svg
-            className="w-5 h-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+        <div className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3">
+          <p
+            href="#"
+            className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-black hover:bg-blue-200 dark:hover:bg-blue-500"
           >
-            <path
-              fill-rule="evenodd"
-              d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-              clip-rule="evenodd"
-            />
-          </svg>
+            <svg
+              className="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                clip-rule="evenodd"
+              />
+            </svg>
 
-          <span className="mt-2">121 Street, NY</span>
-        </p>
+            <span className="mt-2">121 Street, NY</span>
+          </p>
 
-        <p
-          href="#"
-          className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-black hover:bg-blue-200 dark:hover:bg-blue-500"
-        >
-          <svg
-            className="w-5 h-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+          <p
+            href="#"
+            className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-black hover:bg-blue-200 dark:hover:bg-blue-500"
           >
-            <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-          </svg>
+            <svg
+              className="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+            </svg>
 
-          <span className="mt-2">+2499999666600</span>
-        </p>
+            <span className="mt-2">+2499999666600</span>
+          </p>
 
-        <p
-          href="#"
-          className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:black hover:bg-blue-200 dark:hover:bg-blue-500"
-        >
-          <svg
-            className="w-5 h-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+          <p
+            href="#"
+            className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:black hover:bg-blue-200 dark:hover:bg-blue-500"
           >
-            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-          </svg>
+            <svg
+              className="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+              <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+            </svg>
 
-          <span className="mt-2">example@example.com</span>
-        </p>
-      </div>
-
-      <div className="mt-6 ">
-        <div className="items-center -mx-2 md:flex">
-          <div className="w-full mx-2">
-            <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-black">
-              Name
-            </label>
-
-            <input
-            value={user?.displayName || ""}
-              className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              type="text"
-            />
-          </div>
-
-          <div className="w-full mx-2 mt-4 md:mt-0">
-            <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-black">
-              E-mail
-            </label>
-
-            <input
-              value={user?.email || ""}
-              className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              type="email" disabled
-            />
-          </div>
-          <div className="w-full mx-2 mt-4 md:mt-0">
-            <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-black">
-              Education
-            </label>
-
-            <input
-              className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              type="text"
-            />
-          </div>
+            <span className="mt-2">example@example.com</span>
+          </p>
         </div>
 
-        <div className="w-full mt-4">
-          <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-black">
-           Address
-          </label>
+        <div className="mt-6 ">
+          <div className="items-center -mx-2 md:flex">
+            <div className="w-full mx-2">
+              <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-black">
+                Name
+              </label>
 
-          <textarea className="block w-full h-40 px-4 py-2 text-gray-700 bg-white border rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"></textarea>
-        </div>
-        <div className="w-full mt-4">
-          <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-black">
-            City
-          </label>
+              <input
+                value={user?.displayName || ""}
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                type="text"
+              />
+            </div>
 
-          <select className="select select-primary w-full max-w-xs">
-            <option disabled selected>
-              pick your city
-            </option>
-            <option>Dhaka</option>
-            <option>Feni</option>
-            <option>Sylhet</option>
-            <option>Rajshahi</option>
-          </select>
+            <div className="w-full mx-2 mt-4 md:mt-0">
+              <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-black">
+                E-mail
+              </label>
+
+              <input
+                value={user?.email || ""}
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                type="email"
+                disabled
+              />
+            </div>
+            <div className="w-full mx-2 mt-4 md:mt-0">
+              <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-black">
+                Education
+              </label>
+
+              <input
+                name="education"
+                {...register("education")}
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                type="text"
+              />
+            </div>
+          </div>
+
+          <div className="w-full mt-4">
+            <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-black">
+              Address
+            </label>
+
+            <textarea 
+            name="address"
+            {...register("address")}
+            className="block w-full h-40 px-4 py-2 text-gray-700 bg-white border rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"></textarea>
+          </div>
+          <div className="w-full mt-4">
+            <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-black">
+              City
+            </label>
+
+            <select name="city" {...register("city")} className="select select-primary w-full max-w-xs">
+              <option disabled selected>
+                pick your city
+              </option>
+              <option>Dhaka</option>
+              <option>Feni</option>
+              <option>Sylhet</option>
+              <option>Rajshahi</option>
+            </select>
+          </div>
+          <div className="flex justify-center mt-6">
+            <button type="submit" className="px-4 py-2 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Send Message
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
